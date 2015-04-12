9432b10e9ff52fcafbf15bb9af1fbea44ef69183
Christopher Moody <chrisemoody@gmail.com>
2015-04-12 23:02:57 +0000

    Added RMSE test

--- tests/test_ntf.py
+++ tests/test_ntf.py
@@ -1,6 +1,10 @@
 import unittest
 import utils
+import numpy as np
 from ntf import BetaNTF
+
+def rmse(x, y):
+    return np.sqrt((x - y)**2.0).sum()
 
 class TestNTFLib(unittest.TestCase):
     def test_init(self):
@@ -14,6 +18,10 @@ class TestNTFLib(unittest.TestCase):
                 utils.generate_dataset()
         bnf = BetaNTF(shape, n_components=k)
         before = bnf.score(x_indices, x_vals)
+        initial = bnf.impute()
         reconstructed = bnf.fit(x_indices, x_vals)
         after = bnf.score()
         self.assertTrue(after < before)
+        e_0 = rmse(x, initial)
+        e_1 = rmse(x, reconstructed)
+        self.assertTrue(e_0 > e_1)
