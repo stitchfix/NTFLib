fe6d39b4ee38d17c2fe67d226dc202c69e86d45d
Christopher Moody <chrisemoody@gmail.com>
2015-04-12 23:08:30 +0000

    Building python package

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(name='ntflib',
+                 version='0.0.1',
+                 description='Non-negative Sparse Tensor Factorization Library',
+                 long_description=open('README.md').read().strip(),
+                 py_modules=['ntflib'],
+                 install_requires=['numpy'],
+                 zip_safe=True)
